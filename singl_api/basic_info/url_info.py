6866fbfed0c129a5edeca37f7e835989ef1d83b6
py-testers <596805069@qq.com>
2018-11-21 18:15:54 +0800

    update url_info.py about flows

--- singl_api/basic_info/url_info.py
+++ singl_api/basic_info/url_info.py
@@ -24,13 +24,8 @@ query_exectution_url = "%s/api/executions/query" % MY_LOGIN_INFO["HOST"]
 # 批量查询execution
 gQuery_execution_url = "%s/api/executions/groupQuery" % MY_LOGIN_INFO["HOST"]
 
-
-
 # 批量删除schedulers
 delete_schedulers_url = "%s/api/schedulers/removeList" % (MY_LOGIN_INFO["HOST"])
-
-
-
 
 # 查询flow接口
 query_flows_url = "%s/api/flows/query" % (MY_LOGIN_INFO["HOST"])
@@ -43,12 +38,32 @@ query_flowname_version_url = "%s/api/flows/name/%s/%s" % (MY_LOGIN_INFO["HOST"],
 # 查询简化版流程
 query_flow_all_url = '%s/api/flows/all' % (MY_LOGIN_INFO["HOST"])
 # 更新流程
-flow_update_url = '%s/api/flows/%s' % (MY_LOGIN_INFO["HOST"],flow_update_id)
+flow_update_url = '%s/api/flows/%s' % (MY_LOGIN_INFO["HOST"], flow_update_id)
+# 查询需要更新的流程的flowid的url
+flow_update_flowid_url = '%s/api/flows/%s/findFlow' % (MY_LOGIN_INFO["HOST"], flow_update_id)
 # 根据老的版本查询历史流程
-query_flow_history_version_url = '%s/api/flows/history/%s/%s' %(MY_LOGIN_INFO["HOST"], flow_update_id, query_flow_version)
+query_flow_history_version_url = '%s/api/flows/history/%s/%s' % (
+    MY_LOGIN_INFO["HOST"], flow_update_id, query_flow_version)
 # 根据老的id查询历史流程
-query_flow_history_id_url = '%s/api/flows/history/list/%s' %(MY_LOGIN_INFO["HOST"], flow_update_id)
+query_flow_history_id_url = '%s/api/flows/history/list/%s' % (MY_LOGIN_INFO["HOST"], flow_update_id)
 # 根据流程id和计划id查询执行历史
-query_flow_flowAscheduler_id_url = '%s/api/flows/%s/schedulers/%s/executions' % (MY_LOGIN_INFO["HOST"], flow_update_id, flow_scheduler_id)
+query_flow_flowAscheduler_id_url = '%s/api/flows/%s/schedulers/%s/executions' % (
+    MY_LOGIN_INFO["HOST"], flow_update_id, flow_scheduler_id)
 # 根据老的版本查询流程
 query_flow_version_url = '%s/api/flows/%s/%s' % (MY_LOGIN_INFO["HOST"], flow_update_id, query_flow_version)
+# 更新执行计划
+flow_update_schedulers_url = '%s/api/flows/%s/schedulers' % (MY_LOGIN_INFO["HOST"], flow_update_id)
+# 根据id和版本更新流程
+flow_id_version_update_url = '%s/api/flows/update/%s' % (MY_LOGIN_INFO["HOST"], flow_update_id)
+# 更新版本
+flow_name_version_update_url = "%s/api/flows/name/%s/%s" % (MY_LOGIN_INFO["HOST"], query_flow_name, 13)
+# 根据id查询flow
+flow_queryById_url = "%s/api/flows/%s/findFlow" % (MY_LOGIN_INFO["HOST"], flow_update_id)
+# 根据id查询版本
+flow_queryById_Version_url = "%s/api/flows/%s" % (MY_LOGIN_INFO["HOST"], flow_update_id)
+# 查询运行时的一些属性
+query_flow_id_runP_url = "%s/api/flows/%s/runtime-properties" % (MY_LOGIN_INFO["HOST"], flow_update_id)
+# 根据用户名更新流程
+flow_updateByName_url = '%s/api/flows/name/%s' % (MY_LOGIN_INFO["HOST"], query_flow_name)
+# 导出流程
+flow_export_url = '%s/api/flows/export' % (MY_LOGIN_INFO["HOST"])
