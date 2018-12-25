1141aa8181dae5f75b1dfaeb8e0abe3d9eabc9f2
wanganchong <9262623@qq.com>
2018-12-25 18:38:36 +0800

    修改了get_json方法，对其报告输出加了若找不到dataset的条件

--- singl_api/basic_info/get_auth_token.py
+++ singl_api/basic_info/get_auth_token.py
@@ -1,10 +1,10 @@
 import requests
-from basic_info.setting import MY_LOGIN_INFO
+from basic_info.setting import MY_LOGIN_INFO2
 
 
 # 获取登录后返回的X-AUTH-TOKEN
 def get_auth_token():
-    res = requests.post(url=MY_LOGIN_INFO["URL"], headers=MY_LOGIN_INFO["HEADERS"], data=MY_LOGIN_INFO["DATA"])
+    res = requests.post(url=MY_LOGIN_INFO2["URL"], headers=MY_LOGIN_INFO2["HEADERS"], data=MY_LOGIN_INFO2["DATA"])
     x_auth_token = res.headers['X-AUTH-TOKEN']
     # print(x_auth_token)
     return x_auth_token
