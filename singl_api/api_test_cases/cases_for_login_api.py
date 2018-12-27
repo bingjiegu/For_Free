d9b21eeee4d3b8680290302e6500192dab5062c0
wanganchong <9262623@qq.com>
2018-12-27 17:36:03 +0800

    修改了cases_for_login_api文件使用的用户名 换成了admin

--- singl_api/api_test_cases/cases_for_login_api.py
+++ singl_api/api_test_cases/cases_for_login_api.py
@@ -1,5 +1,5 @@
 import unittest
-from basic_info.setting import MY_LOGIN_INFO
+from basic_info.setting import MY_LOGIN_INFO2
 import requests
 import json
 
@@ -8,15 +8,16 @@ class CheckLogin(unittest.TestCase):
     """login 接口测试"""
     """测试登录接口"""
     def setUp(self):
-        self.login_info = MY_LOGIN_INFO["DATA"]
-        self.login_header = MY_LOGIN_INFO["HEADERS"]
+        self.login_info = MY_LOGIN_INFO2["DATA"]
+        self.login_header = MY_LOGIN_INFO2["HEADERS"]
 
     def test_case01(self):
         """正常登录"""
         from basic_info.url_info import login_url
         res = requests.post(url=login_url, headers=self.login_header, data=self.login_info)
         # print(res.status_code, res.text, res.json(), type(res.json()))
-        assert res.status_code == 200
+        self.assertEqual(res.status_code, 200)
+        # assert res.status_code == 200
 
     def test_case02(self):
         """用户名错误"""
@@ -30,7 +31,7 @@ class CheckLogin(unittest.TestCase):
     def test_case03(self):
         """密码错误"""
         from basic_info.url_info import login_url
-        data = {'name': 'gbj_use', 'password': '12345678', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': 'default'}
+        data = {'name': 'admin', 'password': '12345678', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': 'default'}
         res = requests.post(url=login_url, headers=self.login_header, json=data)
         # print(res.status_code, res.text)
         assert res.status_code == 400
@@ -39,7 +40,7 @@ class CheckLogin(unittest.TestCase):
     def test_case04(self):
         """租户错误"""
         from basic_info.url_info import login_url
-        data = {'name': 'gbj_use', 'password': '12345678', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': 'defaul'}
+        data = {'name': 'admin', 'password': '12345678', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': 'defaul'}
         res = requests.post(url=login_url, headers=self.login_header, json=data)
         # print(res.status_code, res.text)
         assert res.status_code == 400
@@ -57,7 +58,7 @@ class CheckLogin(unittest.TestCase):
     def test_case06(self):
         """密码为空"""
         from basic_info.url_info import login_url
-        data = {'name': 'gbj_use', 'password': '', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': 'default'}
+        data = {'name': 'admin', 'password': '', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': 'default'}
         res = requests.post(url=login_url, headers=self.login_header, json=data)
         # print(res.status_code, res.text)
         assert res.status_code == 400
@@ -66,7 +67,7 @@ class CheckLogin(unittest.TestCase):
     def test_case07(self):
         """租户为空"""
         from basic_info.url_info import login_url
-        data = {'name': 'gbj_use', 'password': '123456', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': ''}
+        data = {'name': 'admin', 'password': '123456', 'version': 'Europa-3.0.0.19 - 20180428', 'tenant': ''}
         res = requests.post(url=login_url, headers=self.login_header, json=data)
         # print(res.status_code, res.text)
         assert res.status_code == 400
