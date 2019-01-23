ff172e9468aaebd8f38f431e758c56147185f30c
bingjiegu <bingjie07191223@sina.com>
2019-01-23 18:12:18 +0800

    去掉无语语句

--- singl_api/basic_info/timestamp_13.py
+++ singl_api/basic_info/timestamp_13.py
@@ -3,17 +3,11 @@ import pymysql
 from basic_info import data_from_db
 
 
-def timestamp_to_13(time_stamp, digits=13):
-    time_stamp = data_from_db.schema()
-    time_stamp = time_stamp.strftime('%a %b %d %H:%M:%S %Y')
+def timestamp_to_13(digits=13):
+    # time_stamp = data_from_db.schema()
+    time_stamp = time.strftime('%a %b %d %H:%M:%S %Y')
     time_stamp = time.mktime(time.strptime(time_stamp))
     digits = 10 ** (digits - 10)
     time_stamp = int(round(time_stamp*digits))
     return time_stamp
 
-
-
-
-
-
-print(timestamp_to_13(13))
