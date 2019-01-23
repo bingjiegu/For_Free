5c190486db04dd48214dd47f531ecad5427b694a
bingjiegu <bingjie07191223@sina.com>
2019-01-23 16:31:19 +0800

    恢复创建flow的脚本

--- singl_api/api_test_cases/cases_for_flow_api.py
+++ singl_api/api_test_cases/cases_for_flow_api.py
@@ -16,94 +16,94 @@ ms = MYSQL(MySQL_CONFIG["HOST"], MySQL_CONFIG["USER"], MySQL_CONFIG["PASSWORD"],
 
 
 # 该脚本用来测试创建flow的场景
-# class CreateFlow(unittest.TestCase):
-#     """用来测试创建flow"""
-#     from basic_info.url_info import create_flow_url
-#
-#     def test_case01(self):
-#         """正常创建flow-dataflow"""
-#         flow_name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + 'flow'
-#         data = {"name": flow_name, "flowType": "dataflow", "resource": {"id": "8cb5f399-ec5d-4236-98d3-88f0d1d19d2b"},
-#                 "steps": [], "links": []}
-#         res = requests.post(url=self.create_flow_url, headers=get_headers(), data=json.dumps(data))
-#         # response
-#         response_text = json.loads(res.text)
-#         # 查询创建的flow id, name, type，并组装成一个dict， 和response对比
-#         SQL = 'select id, flow_type from merce_flow where name = "%s"' % flow_name
-#         flow_info = ms.ExecuQuery(SQL)
-#         flow_id = flow_info[0]["id"]
-#         flow_Type = flow_info[0]["flow_type"]
-#         # print(flow_id, flow_Type)
-#         # print(type(response_text), response_text)
-#         self.assertEqual(res.status_code, 200, 'flow创建后返回的status_code不正确')
-#         self.assertEqual(response_text["id"], flow_id, 'flow创建后查询ID不相等')
-#         self.assertEqual(response_text["flowType"], flow_Type, 'flow创建后flow_type不一致')
-#         time.sleep(5)
-#
-#     def test_case02(self):
-#         """正常创建flow-workflow"""
-#         flow_name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + 'workflow'
-#         data = {"name": flow_name, "flowType": "workflow", "resource": {"id": "8cb5f399-ec5d-4236-98d3-88f0d1d19d2b"},
-#                 "steps": [], "links": []}
-#         res = requests.post(url=self.create_flow_url, headers=get_headers(), data=json.dumps(data))
-#         # response
-#         response_text = json.loads(res.text)
-#         # 查询创建的flow id, name, type，并组装成一个dict， 和response对比
-#         SQL = 'select id, flow_type from merce_flow where name = "%s"' % flow_name
-#         flow_info = ms.ExecuQuery(SQL)
-#         flow_id = flow_info[0]["id"]
-#         flow_Type = flow_info[0]["flow_type"]
-#         # print(flow_id, flow_Type)
-#         # print(type(response_text), response_text)
-#         self.assertEqual(res.status_code, 200, 'flow创建后返回的status_code不正确')
-#         self.assertEqual(response_text["id"], flow_id, 'flow创建后查询ID不相等')
-#         self.assertEqual(response_text["flowType"], flow_Type, 'flow创建后flow_type不一致')
-#         time.sleep(5)
-#
-#     def test_case03(self):
-#         """正常创建flow-streamflow"""
-#         flow_name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + 'streamflow'
-#         data = {"name": flow_name, "flowType": "streamflow", "resource": {"id": "8cb5f399-ec5d-4236-98d3-88f0d1d19d2b"},
-#                 "steps": [], "links": []}
-#         res = requests.post(url=self.create_flow_url, headers=get_headers(), data=json.dumps(data))
-#         # response
-#         response_text = json.loads(res.text)
-#         # 查询创建的flow id, name, type，并组装成一个dict， 和response对比
-#         SQL = 'select id, flow_type from merce_flow where name = "%s"' % flow_name
-#         flow_info = ms.ExecuQuery(SQL)
-#         flow_id = flow_info[0]["id"]
-#         flow_Type = flow_info[0]["flow_type"]
-#         # print(flow_id, flow_Type)
-#         # print(type(response_text), response_text)
-#         self.assertEqual(res.status_code, 200, 'flow创建后返回的status_code不正确')
-#         self.assertEqual(response_text["id"], flow_id, 'flow创建后查询ID不相等')
-#         self.assertEqual(response_text["flowType"], flow_Type, 'flow创建后flow_type不一致')
-#         time.sleep(3)
-#
-#
-# class GetDataSet(unittest.TestCase):
-#     """测试dataset查询接口"""
-#
-#     def test_case01(self):
-#         """使用id查询"""
-#         try:
-#             dataset_sql = 'select id, name from merce_dataset order by create_time desc limit 1'
-#             dataset_info = ms.ExecuQuery(dataset_sql)
-#             dataset_id = dataset_info[0]["id"]
-#             dataset_name = dataset_info[0]["name"]
-#             # print(type(dataset_id[0][0]))
-#         except Exception as e:
-#             raise e
-#         else:
-#             url2 = '%s/api/datasets/%s?tenant=%s' % (MY_LOGIN_INFO["HOST"], dataset_id, tenant_id)
-#             response = requests.get(url=url2, headers=get_headers()).text
-#             response = json.loads(response)
-#             response_id = response["id"]
-#             response_name = response["name"]
-#             # print("id:", response["id"])
-#             # print({"id": dataset_id, "name": dataset_name} == {"id": response_id, "name": response_name})
-#             self.assertEqual({"id": dataset_id, "name": dataset_name}, {"id": response_id, "name": response_name},
-#                              '两次查询得到的dataset id和name不一致，查询失败')
+class CreateFlow(unittest.TestCase):
+    """用来测试创建flow"""
+    from basic_info.url_info import create_flow_url
+
+    def test_case01(self):
+        """正常创建flow-dataflow"""
+        flow_name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + 'flow'
+        data = {"name": flow_name, "flowType": "dataflow", "resource": {"id": "8cb5f399-ec5d-4236-98d3-88f0d1d19d2b"},
+                "steps": [], "links": []}
+        res = requests.post(url=self.create_flow_url, headers=get_headers(), data=json.dumps(data))
+        # response
+        response_text = json.loads(res.text)
+        # 查询创建的flow id, name, type，并组装成一个dict， 和response对比
+        SQL = 'select id, flow_type from merce_flow where name = "%s"' % flow_name
+        flow_info = ms.ExecuQuery(SQL)
+        flow_id = flow_info[0]["id"]
+        flow_Type = flow_info[0]["flow_type"]
+        # print(flow_id, flow_Type)
+        # print(type(response_text), response_text)
+        self.assertEqual(res.status_code, 200, 'flow创建后返回的status_code不正确')
+        self.assertEqual(response_text["id"], flow_id, 'flow创建后查询ID不相等')
+        self.assertEqual(response_text["flowType"], flow_Type, 'flow创建后flow_type不一致')
+        time.sleep(5)
+
+    def test_case02(self):
+        """正常创建flow-workflow"""
+        flow_name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + 'workflow'
+        data = {"name": flow_name, "flowType": "workflow", "resource": {"id": "8cb5f399-ec5d-4236-98d3-88f0d1d19d2b"},
+                "steps": [], "links": []}
+        res = requests.post(url=self.create_flow_url, headers=get_headers(), data=json.dumps(data))
+        # response
+        response_text = json.loads(res.text)
+        # 查询创建的flow id, name, type，并组装成一个dict， 和response对比
+        SQL = 'select id, flow_type from merce_flow where name = "%s"' % flow_name
+        flow_info = ms.ExecuQuery(SQL)
+        flow_id = flow_info[0]["id"]
+        flow_Type = flow_info[0]["flow_type"]
+        # print(flow_id, flow_Type)
+        # print(type(response_text), response_text)
+        self.assertEqual(res.status_code, 200, 'flow创建后返回的status_code不正确')
+        self.assertEqual(response_text["id"], flow_id, 'flow创建后查询ID不相等')
+        self.assertEqual(response_text["flowType"], flow_Type, 'flow创建后flow_type不一致')
+        time.sleep(5)
+
+    def test_case03(self):
+        """正常创建flow-streamflow"""
+        flow_name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + 'streamflow'
+        data = {"name": flow_name, "flowType": "streamflow", "resource": {"id": "8cb5f399-ec5d-4236-98d3-88f0d1d19d2b"},
+                "steps": [], "links": []}
+        res = requests.post(url=self.create_flow_url, headers=get_headers(), data=json.dumps(data))
+        # response
+        response_text = json.loads(res.text)
+        # 查询创建的flow id, name, type，并组装成一个dict， 和response对比
+        SQL = 'select id, flow_type from merce_flow where name = "%s"' % flow_name
+        flow_info = ms.ExecuQuery(SQL)
+        flow_id = flow_info[0]["id"]
+        flow_Type = flow_info[0]["flow_type"]
+        # print(flow_id, flow_Type)
+        # print(type(response_text), response_text)
+        self.assertEqual(res.status_code, 200, 'flow创建后返回的status_code不正确')
+        self.assertEqual(response_text["id"], flow_id, 'flow创建后查询ID不相等')
+        self.assertEqual(response_text["flowType"], flow_Type, 'flow创建后flow_type不一致')
+        time.sleep(3)
+
+
+class GetDataSet(unittest.TestCase):
+    """测试dataset查询接口"""
+
+    def test_case01(self):
+        """使用id查询"""
+        try:
+            dataset_sql = 'select id, name from merce_dataset order by create_time desc limit 1'
+            dataset_info = ms.ExecuQuery(dataset_sql)
+            dataset_id = dataset_info[0]["id"]
+            dataset_name = dataset_info[0]["name"]
+            # print(type(dataset_id[0][0]))
+        except Exception as e:
+            raise e
+        else:
+            url2 = '%s/api/datasets/%s?tenant=%s' % (MY_LOGIN_INFO["HOST"], dataset_id, tenant_id)
+            response = requests.get(url=url2, headers=get_headers()).text
+            response = json.loads(response)
+            response_id = response["id"]
+            response_name = response["name"]
+            # print("id:", response["id"])
+            # print({"id": dataset_id, "name": dataset_name} == {"id": response_id, "name": response_name})
+            self.assertEqual({"id": dataset_id, "name": dataset_name}, {"id": response_id, "name": response_name},
+                             '两次查询得到的dataset id和name不一致，查询失败')
 
 
 class ApiFlows(unittest.TestCase):
