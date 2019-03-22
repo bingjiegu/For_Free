e7b42412ffc734aa7eb85d7e4972bbe32dc96488
bingjiegu <bingjie07191223@sina.com>
2019-03-22 12:12:14 +0800

    token update

--- singl_api/basic_info/get_auth_token.py
+++ singl_api/basic_info/get_auth_token.py
@@ -5,7 +5,9 @@ from basic_info.setting import MY_LOGIN_INFO2
 
 # 获取登录后返回的X-AUTH-TOKEN
 def get_auth_token():
+    print('-----',MY_LOGIN_INFO2)
     res = requests.post(url=MY_LOGIN_INFO2["URL"], headers=MY_LOGIN_INFO2["HEADERS"], data=MY_LOGIN_INFO2["DATA"])
+
     print('1:', type(res.headers))
     dict_headers = dict(res.headers)
     print('2:', type(dict_headers))
@@ -19,9 +21,11 @@ def get_auth_token():
 def get_headers():
     x_auth_token = get_auth_token()
     headers = {'Content-Type': 'application/json', "X-AUTH-TOKEN": x_auth_token, "Accept": "application/json"}
+    print(headers)
     return headers
 
 
-
+get_auth_token()
+get_headers()
 
 
