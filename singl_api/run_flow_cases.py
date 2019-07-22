89c83e92d229a8a25ab25c0afa7740605f75b30d
bingjiegu <bingjie07191223@sina.com>
2019-07-22 10:37:26 +0800

    将run.py重命名为run_flow_cases.py

--- singl_api/run_flow_cases.py
+++ singl_api/run_flow_cases.py
@@ -7,7 +7,7 @@ from basic_info.get_auth_token import get_headers
 from api_test_cases.get_execution_output_json import GetCheckoutDataSet
 import threading
 import datetime
-from new_api_cases.execute_cases import deal_request_method, CheckResult
+# from new_api_cases.execute_cases import deal_request_method, CheckResult
 # from newSuite import NewSuite
 
 # 添加用例集的API用例。暂停
@@ -21,22 +21,22 @@ filename = time.strftime("%Y%m%d%H", time.localtime()) + '_report.html'
 # report_path = 'E:\Reports\\' + filename
 # report_path = '/root/gbj/Reports/' + filename  # 192.168.1.87环境Jenkins使用
 report_path = './Reports/' + filename  # 192.168.1.87环境Jenkins使用
-fp = open(report_path, 'wb')
-runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='API自动化测试报告', description='覆盖dataset,schema,schedulers,execution等测试场景')
-print('开始执行用例集用例')
-runner.run(testcase)
-fp.close()
+# fp = open(report_path, 'wb')
+# runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='API自动化测试报告', description='覆盖dataset,schema,schedulers,execution等测试场景')
+# print('开始执行用例集用例')
+# runner.run(testcase)
+# fp.close()
 print('------开始执行用例-------')
 start_time = datetime.datetime.now()
 print('------开始执行flow用例------')
 # 执行flow用例
-# obj = GetCheckoutDataSet()
-# sink_dataet_json = obj.get_json()
+obj = GetCheckoutDataSet()
+sink_dataet_json = obj.get_json()
 print('------开始执行api case------')
 # 执行API用例
-deal_request_method()
-# 对比API用例结果
-CheckResult().deal_result()
+# deal_request_method()
+# # 对比API用例结果
+# CheckResult().deal_result()
 # 发送邮件
 print('用例执行完毕，开始发送邮件')
 main3()
